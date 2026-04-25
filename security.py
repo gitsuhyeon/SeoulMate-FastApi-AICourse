c795f2a65143ad7adc4418689e9cff764a18e7a0
gitsuhyeon <140365503+gitsuhyeon@users.noreply.github.com>
2026-04-25 14:09:33 +0900

    서버에서 잘 들어오는지 확인

--- security.py
+++ security.py
@@ -10,11 +10,11 @@ API_KEY_NAME = "X-API-Key"
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
 
 # 실서버 배포 시 Render 환경변수에 FASTAPI_SECRET_KEY를 등록하세요.
-FASTAPI_SECRET_KEY = os.getenv("FASTAPI_SECRET_KEY")
+FASTAPI_SECRET_KEY = os.getenv("AI_SERVER_SECRET")
 
 async def verify_api_key(api_key: str = Security(api_key_header)):
     """API Key를 검증하는 전역 문지기 함수"""
-    logger.warning(f"수신된 API KEY: '{api_key}', 길이: {len(api_key)}")#문제 없으면 삭제해
+    #logger.warning(f"수신된 API KEY: '{api_key}', 길이: {len(api_key)}")#문제 없으면 삭제해
     if api_key == FASTAPI_SECRET_KEY:
         return api_key
     
